44f18995e320de4d2b345c51d173e70e4bb87bb1
jihaduddinjbm <jihaduddin117@gmail.com>
2023-10-12 21:28:52 +0600

    added a new jsx file

---
+++ src/pages/Home/BreakingNews.jsx
@@ -0,0 +1,11 @@
+
+
+const BreakingNews = () => {
+    return (
+        <div>
+            <button className="btn btn-secondary">Breaking news</button>
+        </div>
+    );
+};
+
+export default BreakingNews;
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,11 +1,12 @@
 import logo from '../../../assets/assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-         <img src={logo} alt="" />
+        <div className='text-center'>
+         <img className='mx-auto' src={logo} alt="" />
          <p>Journalism Without Fear or Favour</p>
-         <p className='text-lg'>Sunday, November 27, 2022</p>
+         <p className='text-lg'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
